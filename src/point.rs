37e0a9b24696c1f25c91ed017b1f09d96050a3dd
Fredrik Larsson <fredrikln@gmail.com>
2019-10-28 12:30:17 +0100

    - Added float comparison lib - Vector: multiplying, dividing, normalization, magnitude, dot product, cross product, negation

--- src/point.rs
+++ src/point.rs
@@ -1,5 +1,6 @@
 use std::ops::{Add, Sub};
 use crate::vector::Vector;
+use float_cmp::approx_eq;
 
 #[derive(Debug)]
 pub struct Point {
@@ -10,9 +11,7 @@ pub struct Point {
 
 impl PartialEq for Point {
   fn eq(&self, other: &Self) -> bool {
-    let epsilon = std::f32::EPSILON;
-
-    ((self.x-other.x).abs() < epsilon) && ((self.y-other.y).abs() < epsilon) && ((self.z-other.z).abs() < epsilon)
+    approx_eq!(f32, self.x, other.x) && approx_eq!(f32, self.y, other.y) && approx_eq!(f32, self.z, other.z)
   }
 }
 
@@ -70,17 +69,14 @@ mod tests {
   fn can_be_compared() {
     let p1 = Point { x: 1.0, y: 2.0, z: 3.0 };
     let p2 = Point { x: 1.0, y: 2.0, z: 3.0 };
-
     assert_eq!(p1, p2);
 
     let p3 = Point { x: 3.0, y: 2.0, z: 3.0 };
     let p4 = Point { x: 1.0, y: 2.0, z: 3.0 };
-
     assert_ne!(p3, p4);
 
     let p5 = Point { x: 0.15 + 0.15 + 0.15, y: 1.0, z: 1.0 };
     let p6 = Point { x: 0.1 + 0.1 + 0.25, y: 1.0, z: 1.0 };
-
     assert_eq!(p5, p6);
   }
 
@@ -88,7 +84,6 @@ mod tests {
   fn can_be_added() {
     let p1 = Point { x: 1.0, y: 2.0, z: 3.0 };
     let v1 = Vector { x: 1.0, y: 2.0, z: 3.0 };
-
     assert_eq!(p1 + v1, Point { x: 2.0, y: 4.0, z: 6.0 });
   }
 
@@ -96,12 +91,10 @@ mod tests {
   fn can_be_subtracted() {
     let p1 = Point { x: 1.0, y: 2.0, z: 3.0 };
     let p2 = Point { x: 1.0, y: 2.0, z: 3.0 };
-
     assert_eq!(p1 - p2, Vector { x: 0.0, y: 0.0, z: 0.0 });
 
     let p3 = Point { x: 1.0, y: 2.0, z: 3.0 };
     let v1 = Vector { x: 1.0, y: 2.0, z: 3.0 };
-
     assert_eq!(p3 - v1, Point { x: 0.0, y: 0.0, z: 0.0 });
   }
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,4 +1,5 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Sub, Neg, Mul, Div};
+use float_cmp::approx_eq;
 
 #[derive(Debug)]
 pub struct Vector {
@@ -7,11 +8,36 @@ pub struct Vector {
   pub z: f32,
 }
 
+impl Vector {
+  pub fn magnitude(&self) -> f32 {
+    (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+  }
+
+  pub fn normalize(&self) -> Vector {
+    let magnitude = self.magnitude();
+    Vector {
+      x: self.x / magnitude,
+      y: self.y / magnitude,
+      z: self.z / magnitude,
+    }
+  }
+
+  pub fn dot(&self, other: &Self) -> f32 {
+    self.x * other.x + self.y * other.y + self.z * other.z
+  }
+
+  pub fn cross(&self, other: &Self) -> Vector {
+    Vector {
+      x: self.y * other.z - self.z * other.y,
+      y: self.z * other.x - self.x * other.z,
+      z: self.x * other.y - self.y * other.x,
+    }
+  }
+}
+
 impl PartialEq for Vector {
   fn eq(&self, other: &Self) -> bool {
-    let epsilon = std::f32::EPSILON;
-
-    ((self.x-other.x).abs() < epsilon) && ((self.y-other.y).abs() < epsilon) && ((self.z-other.z).abs() < epsilon)
+    approx_eq!(f32, self.x, other.x) && approx_eq!(f32, self.y, other.y) && approx_eq!(f32, self.z, other.z)
   }
 }
 
@@ -39,9 +65,58 @@ impl Sub<Vector> for Vector {
   }
 }
 
+impl Neg for Vector {
+  type Output = Vector;
+
+  fn neg(self) -> Vector {
+    Vector {
+      x: -self.x,
+      y: -self.y,
+      z: -self.z,
+    }
+  }
+}
+
+impl Mul<f32> for Vector {
+  type Output = Vector;
+
+  fn mul(self, rhs: f32) -> Vector {
+    Vector {
+      x: self.x * rhs,
+      y: self.y * rhs,
+      z: self.z * rhs,
+    }
+  }
+}
+
+impl Mul<Vector> for Vector {
+  type Output = Vector;
+
+  fn mul(self, other: Self) -> Vector {
+    Vector {
+      x: self.x * other.x,
+      y: self.y * other.y,
+      z: self.z * other.z,
+    }
+  }
+}
+
+impl Div<f32> for Vector {
+  type Output = Vector;
+
+  fn div(self, rhs: f32) -> Vector {
+    Vector {
+      x: self.x / rhs,
+      y: self.y / rhs,
+      z: self.z / rhs,
+    }
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use crate::vector::Vector;
+  use float_cmp::approx_eq;
 
   #[test]
   fn can_be_instanciated() {
@@ -56,17 +131,14 @@ mod tests {
   fn can_be_compared() {
     let v1 = Vector { x: 1.0, y: 2.0, z: 3.0 };
     let v2 = Vector { x: 1.0, y: 2.0, z: 3.0 };
-
     assert_eq!(v1, v2);
 
     let v3 = Vector { x: 3.0, y: 2.0, z: 3.0 };
     let v4 = Vector { x: 1.0, y: 2.0, z: 3.0 };
-
     assert_ne!(v3, v4);
 
     let v5 = Vector { x: 0.15 + 0.15 + 0.15, y: 1.0, z: 1.0 };
     let v6 = Vector { x: 0.1 + 0.1 + 0.25, y: 1.0, z: 1.0 };
-
     assert_eq!(v5, v6);
   }
 
@@ -74,7 +146,6 @@ mod tests {
   fn can_be_added() {
     let v1 = Vector { x: 1.0, y: 2.0, z: 3.0 };
     let v2 = Vector { x: 1.0, y: 2.0, z: 3.0 };
-
     assert_eq!(v1 + v2, Vector { x: 2.0, y: 4.0, z: 6.0 });
   }
 
@@ -82,7 +153,87 @@ mod tests {
   fn can_be_subtracted() {
     let v1 = Vector { x: 1.0, y: 2.0, z: 3.0 };
     let v2 = Vector { x: 1.0, y: 2.0, z: 3.0 };
-
     assert_eq!(v1 - v2, Vector { x: 0.0, y: 0.0, z: 0.0 });
+  }
+
+  #[test]
+  fn can_be_negated() {
+    let v1 = Vector { x: 1.0, y: 2.0, z: 3.0 };
+    assert_eq!(-v1, Vector { x: -1.0, y: -2.0, z: -3.0 });
+  }
+
+  #[test]
+  fn can_be_multiplied() {
+    let v1 = Vector { x: 1.0, y: 2.0, z: 3.0 };
+    assert_eq!(v1 * 3.0, Vector { x: 3.0, y: 6.0, z: 9.0 });
+
+    let v2 = Vector { x: 1.0, y: 2.0, z: 3.0 };
+    assert_eq!(v2 * 0.5, Vector { x: 0.5, y: 1.0, z: 1.5 });
+  }
+
+  #[test]
+  fn can_be_divided() {
+    let v1 = Vector { x: 3.0, y: 6.0, z: 9.0 };
+    assert_eq!(v1 / 3.0, Vector { x: 1.0, y: 2.0, z: 3.0 });
+
+    let v2 = Vector { x: 0.5, y: 1.0, z: 1.5 };
+    assert_eq!(v2 / 0.5, Vector { x: 1.0, y: 2.0, z: 3.0 });
+  }
+
+  #[test]
+  fn can_get_magnitude() {
+    let v1 = Vector { x: 1.0, y: 0.0, z: 0.0 };
+    assert_eq!(v1.magnitude(), 1.0);
+
+    let v2 = Vector { x: 0.0, y: 1.0, z: 0.0 };
+    assert_eq!(v2.magnitude(), 1.0);
+
+    let v3 = Vector { x: 0.0, y: 0.0, z: 1.0 };
+    assert_eq!(v3.magnitude(), 1.0);
+
+    let v4 = Vector { x: 1.0, y: 2.0, z: 3.0 };
+    let result: f32 = 14.0;
+    assert_eq!(v4.magnitude(), result.sqrt());
+
+    let v5 = Vector { x: -1.0, y: -2.0, z: -3.0 };
+    let result2: f32 = 14.0;
+    assert_eq!(v5.magnitude(), result2.sqrt());
+  }
+
+  #[test]
+  fn can_be_normalized() {
+    let v1 = Vector { x: 4.0, y: 0.0, z: 0.0 };
+    assert_eq!(v1.normalize(), Vector { x: 1.0, y: 0.0, z: 0.0 });
+
+    let v2 = Vector { x: 1.0, y: 2.0, z: 3.0 };
+    assert_eq!(v2.normalize(), Vector { x: 0.26726124, y: 0.5345225, z: 0.8017837 });
+
+    let v3 = Vector { x: 1.0, y: 2.0, z: 3.0 };
+    let result: f32 = v3.normalize().magnitude();
+
+    assert!(approx_eq!(f32, result, 1.0));
+  }
+
+  #[test]
+  fn computes_dot_product() {
+    let v1 = Vector { x: 1.0, y: 2.0, z: 3.0 };
+    let v2 = Vector { x: 2.0, y: 3.0, z: 4.0 };
+
+    assert_eq!(v1.dot(&v2), 20.0);
+    assert_eq!(v2.dot(&v1), 20.0);
+  }
+
+  #[test]
+  fn computes_cross_product() {
+    let v1 = Vector { x: 1.0, y: 2.0, z: 3.0 };
+    let v2 = Vector { x: 2.0, y: 3.0, z: 4.0 };
+
+    assert_eq!(v1.cross(&v2), Vector { x: -1.0, y: 2.0, z: -1.0 });
+    assert_eq!(v2.cross(&v1), Vector { x: 1.0, y: -2.0, z: 1.0 });
+
+    let v3 = Vector { x: 1.0, y: 0.0, z: 0.0 };
+    let v4 = Vector { x: 0.0, y: 1.0, z: 0.0 };
+
+    assert_eq!(v3.cross(&v4), Vector { x: 0.0, y: 0.0, z: 1.0 });
   }
 }
